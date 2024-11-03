e88d99bb72625ce0811a505caf16360e8d519473
Stefano Carraro <stefano.c@edu.pucrs.br>
2024-11-03 14:20:04 -0300

    Added 404 exception when user get by id finds nothing

--- src/main/java/stefano/course/services/UserService.java
+++ src/main/java/stefano/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import stefano.course.entities.User;
 import stefano.course.repositories.UserRepository;
+import stefano.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 
     public User findById(Long id){
         Optional<User> obj = userRepository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(()-> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj){
